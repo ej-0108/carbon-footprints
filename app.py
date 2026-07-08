a3091b41df9ba55176c118f1ed0b8117b4d48f46
ej-0108 <moam09@naver.com>
2026-07-08 22:45:46 +0900

    스트림릿 환경 설정 및 웹사이트 이름, 소개글(10%) 작성
    .

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+# 웹페이지 기본 설정
+st.set_page_config(page_title="클래스 스텝 업 / 기후 데이터 분석", layout="centered")
+#웹사이트 타이틀 및 설명
+st.title("🌱")
+st.write("기후 위기, 얼마나 아시나요?
+         
